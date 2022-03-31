c05b9e7ec194b09d87fb997fdae2de0cc7cfe343
g-vdn <vaideanugeorge@gmail.com>
2022-03-31 03:25:05 +0200

    Update cronjobs.sh

--- cronjobs.sh
+++ cronjobs.sh
@@ -11,73 +11,142 @@
 # Bash Version      : 5.0.3(1)-release
 # ====================================================================================================
 
-####################################################
-# Change below 3 variables according to your needs #
-####################################################
-
-# Use '-i' option to install as alias in user .bashrc: ./cronjobs -i
+###############################################################
+# If required, change below variables according to your needs #
+###############################################################
 
 BASHRC="${HOME}/.bashrc" # Change if .bashrc file is located elsewhere
-ALIAS_NAME='cronjobs' # Name with which you want to run the script
-TODAY="$(date +%d-%B-%Y)" # Change date format if required, use `man date` to see other date possibilities
-FIRST='1' # will display a message first time the script is run. To reset, change value 0
 
+ALIAS_NAME='cronjobs' # Name with which you want to run the script, after it is installed as alias
 
+TODAY="$(date +%d-%b-%Y)" # Date will be insterted in the header before the alias is written to bashrc file.
+                          # Change date format if required, use `man date` to see other date possibilities
+
+FIRST='0' # will display a message first time the script is run. To reset, change value to 0
 
 ##############################################################
 # Do not modify anything under this line or script may break #
 ##############################################################
 
+REQ=("realpath" "basename" "printf" "whoami" "tput") # required installed commands
 
-# Inform about posibility to install as an alias
+sep() {
+    # custom separator, screen width
+    for i in $(seq $(tput cols));do echo -n "${1}";done
+}
+
+usage() {
+    # shows the usage
+sep '+'
+printf "\
+%-s\n\n\
+%-s\n\
+%-s\n\
+%-s\t%-s\n\
+%-s\t%-s\n\
+" \
+"Usage: $(basename ${0}) [-iv]" \
+"Run without any options will print all installed crontabs - user must be in sudoers or script to be run with sudo" \
+"Options:" \
+"[-i]" "Install as alias for user - user must be in sudoers file." \
+"[-h]" "Print this help."
+sep '+'
+}
+
+# If some required commands not found on system, store them in MIS
+for i in ${!REQ[@]}; do
+    if ! type ${REQ[$i]} &> /dev/null; then
+        MIS+=("${REQ[$i]}")
+    fi
+done
+
+# If MIS created, means that some required commands not installed. Inform user and exit
+if (( ${MIS+1} )); then
+    echo "Error: One or more required commands not found on system: '${MIS[@]}'" >&2
+    echo "Install them and retry the script." >&2
+    exit 1
+fi
+
+# Check if user can run commands with sudo 
+if ! $(sudo -nl &> /dev/null || sudo -nv &> /dev/null); then
+    echo "Error: User '$(whoami)' cannot run commands with sudo, stopping script." >&2
+    exit 1
+fi
+
+# Show one time message
 if [[ ${FIRST} = '0' ]]; then
-    printf "%-s\n" "NOTE: This script will show the set-up cron jobs for all users on this system, regardless of their nature. Sudo privileges requiered. For ease of use, you can run the script with '-i' option to install as alias: '${0} -i' . If required, you can modify the variables inside the script file. This message will show only one time."
-    read -p "Press enter to acknowledge and continue..."
+sep '/'
+printf "\
+%-s\n\
+%-s\n\
+%-s\n\
+%-s\n\
+%-s\n\
+%-s\n\
+%-s\n\
+" \
+"NOTE:" \
+"- Usage: $(basename ${0}) -h" \
+"- This script will print on screen installed crontabs for all users on this system, regardless of their nature." \
+"- For the script to run successfully, user must be in sudoers file or to be able to execute commands with sudo." \
+"- For ease of use, you can run the script with '-i' option to install as alias: '${0} -i'" \
+"- If required, you can modify the variables inside the script file." \
+"- This message will not be shown on future runs. To show it again, modify FIRST variable in script to '0'."
+sep '/'
+    read -p "Press ENTER to acknowledge and continue or CTRL + C to cancel"
     sed -i "0,/FIRST='0'/ s//FIRST='1'/" $(basename ${0})
     exit 0
 fi
 
+# Get the user option
+while getopts ih OPTION; do
+    case ${OPTION} in
+        i) INSTALL='true' ;;
+        h) usage 
+           exit 0
+           ;;
+        *) usage 
+           exit 1
+           ;;
+    esac
+done
 
-
-# Checks
-
-
-# Install as alias if -i option is used, append lines in bashrc user file
-if [[ ${1} = '-i' ]]; then
+# Append lines in bashrc file if -i option was used 
+if [[ "${INSTALL}" = 'true' ]]; then
     if [[ -w ${BASHRC} ]]; then
-        printf "%-s\n%-s\n" "########## Custom Aliases ${TODAY} ##########" "alias ${ALIAS_NAME}='/opt/scripts/cronjobs.sh'" >> ${BASHRC} && source ${BASHRC}
-        printf "%-s\n" "Successfully installed as alias. Now you can run cronjobs command from everywhere."
-        exit 0
+        if ! grep -q "Custom Aliases" "${BASHRC}" && ! grep -q "${ALIAS_NAME}" "${BASHRC}"; then     
+            printf "\n%-s\n\n" "Following lines were added to user bashrc file:" 
+            printf "%-s\n%-s\n%-s\n\n" \
+            "########## Custom Aliases ${TODAY} ##########" \
+            "alias ${ALIAS_NAME}='$(realpath ${0})'" \
+            "#########################################" | tee -a ${BASHRC}
+            printf "%-s\n" "Important: Due to shell limitations, there is needed to manually execute command: 'source ${BASHRC}' or restart shell."
+            exit 0
+        else
+            echo "Error: bashrc file already contains alias entries related to this script. Please manually check the file at ${BASHRC}" >&2
+            exit 1
+        fi
     else
-        printf "%-s\n%-s" "File located at '${BASHRC}' non-existent or not writable by you." \
-        "Check BASHRC variable in script $(realpath ${0})"
-    fi
-else
-    if (( ${1+1} )); then
-        printf "%-s\n" "Invalid option. Only -i is accepted which will install " >&2
+        printf "%-s\n%-s\n" "Error: File located at '${BASHRC}' non-existent or not writable by you." \
+        "Check BASHRC variable in script $(realpath ${0})" >&2
         exit 1
     fi
 fi
 
-# Make sure sudo 
-if [[ ${UID} -ne 0 ]]; then
-    echo "Script must be run as root or with sudo privileges" >&2
-    exit 1
-fi
-
-
-
 # Actual script
-
-
-SEPARATOR="$(for i in $(seq $(tput cols));do echo -n "-";done)"
-
+HT='Installed crontabs on this system' # Header Title
+MID="$(( $(( $( tput cols) / 2 )) - $(( ${#HT} / 2 )) ))" # get the start column for the header message
+sep '='
+printf "%-${MID}s%-s\n" "" "${HT}" 
+sep '='
+echo
 cut -d ":" -f 1 /etc/passwd | \
 while read USER; do
-	if [[ "$(sudo -u $USER crontab -l 2>&1 > /dev/null)" != no* ]]; then
-		echo $SEPARATOR
+	if sudo -u $USER crontab -l &> /dev/null; then
+        sep '-'
 		printf "\e[34mCrontabs for user: \e[0m$USER\n"
 		sudo -u $USER crontab -l | sed 's/^\s*#.*//g;s/^\s*MAILTO.*//g;/^$/d'
-		echo $SEPARATOR && echo
+        sep '-'
+        echo
 	fi
 done
